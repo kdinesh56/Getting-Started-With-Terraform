c8385d2231e162b430db50fabc885431699542ad
kdinesh56 <kdinesh56@gmail.com>
2024-05-16 15:52:05 +0530

    removed credentials

--- Create-EC2-Instance/main.tf
+++ Create-EC2-Instance/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIA6ID4BMU4Z4TOELPG"
-  secret_key = "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"
+  access_key = "AAAAA"
+  secret_key = "AAA"
 }
 
 resource "aws_instance" "EC2-Instance-via-Terraform" {
